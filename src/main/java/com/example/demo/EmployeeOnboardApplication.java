e7bbae1fd65b384f2c2b33d0c639aea1bf9114ca
Salian <Amrutha.Salian@randstaddigital.com>
2024-11-19 15:49:09 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/EmployeeOnboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeOnboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeOnboardApplication.class, args);
+	}
+
+}
